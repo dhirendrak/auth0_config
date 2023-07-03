a7b47898c60abdd81d0ef3c5978d5760a11738ca
Dhirendra Kumar <dhiru.m25@gmail.com>
2023-07-03 20:27:28 +0530

    Added a new comment

--- local/rules/Add my custom claim.js
+++ local/rules/Add my custom claim.js
@@ -1,4 +1,5 @@
 function (user, context, callback) {
-  context.accessToken.myCustomClaim = configuration.MY_CUSTOM_DATA;  
+  context.accessToken.myCustomClaim = configuration.MY_CUSTOM_DATA; 
+  // added a comment here
   return callback(null, user, context);
 }
